8fe5213fd23ce71165f956f51d7b7b846fb210f0
AbadCX311 <jesus.ojeda.311@gmail.com>
2024-11-18 17:15:03 -0500

    Added Followers Count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,22 @@
+// Initialize count to 0
+let count = 0;
+
+ // Increment the count by 1.
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+// Display the count in the HTML
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue() {
+    if (count === 10) {
+      alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+      alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
